abd101f289b8ebdf2172dda85252b7eb11d42dd0
fortyseven <fortyseven@gmail.com>
2015-05-23 06:49:24 -0400

    More descriptive vars; removed unused/unimplemented

--- js/drifter.js
+++ js/drifter.js
@@ -1,42 +1,38 @@
 const NUM_FLAKES = 100;
 const FRAME_DELAY = 75;
 
-var xpos = 50;
-var fg = null, fg2 = null;
-
 var snow_x = new Array( NUM_FLAKES );
 var snow_xvel = new Array( NUM_FLAKES );
 var snow_y = new Array( NUM_FLAKES );
 var snow_yvel = new Array( NUM_FLAKES );
-var snow_div = new Array( NUM_FLAKES );
+
 var snow_size = new Array( NUM_FLAKES );
-var snow_twist = new Array( NUM_FLAKES );
-var c = 0;
-var width = 0, height = 0;
+
+var window_width = 0, window_height = 0;
 
 var ctx = null;
 var canvas = null;
 
+var c = 0; // Incremented each updateMove loop
+
 $( document ).ready( function ()
 {
-    width = $( window ).width();
-    height = $( window ).height();
+    window_width = $( window ).width();
+    window_height = $( window ).height();
 
-    canvas = $( "<canvas style='position:absolute; z-index:999; top:0; left: 0' width='" + width + "' height='" + height + "'></canvas>" )[ 0 ];
+    canvas = $( "<canvas style='position:absolute; z-index:999; top:0; left: 0' width='" + window_width + "' height='" + window_height + "'></canvas>" )[ 0 ];
 
     $( "body" ).prepend( canvas );
 
     ctx = canvas.getContext( "2d" );
 
     for ( var i = 0; i < NUM_FLAKES; i++ ) {
-        snow_x[ i ] = Math.round( Math.random() * width );
-        snow_y[ i ] = Math.round( Math.random() * height );
+        snow_x[ i ] = Math.round( Math.random() * window_width );
+        snow_y[ i ] = Math.round( Math.random() * window_height );
         snow_xvel[ i ] = (5 + (Math.random() * 20));
         snow_yvel[ i ] = (5 + (Math.random() * 20));
 
         snow_size[ i ] = Math.round( 1 + (Math.random() * 16) );
-
-        snow_twist[ i ] = 0;
     }
 
     setInterval( updateMove, FRAME_DELAY );
@@ -52,12 +48,12 @@ function updateMove()
         wo = Math.sin( c * 0.1 ) * (Math.random() * 5);
 
         snow_x[ i ] += (snow_xvel[ i ] + wo);
-        if ( snow_x[ i ] > (width - snow_size[ i ]) ) {
+        if ( snow_x[ i ] > (window_width - snow_size[ i ]) ) {
             snow_x[ i ] = 0;
         }
 
         snow_y[ i ] += snow_yvel[ i ];
-        if ( snow_y[ i ] > (height - snow_size[ i ]) ) {
+        if ( snow_y[ i ] > (window_height - snow_size[ i ]) ) {
             snow_y[ i ] = 0;
         }
 
