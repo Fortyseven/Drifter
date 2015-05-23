5d13442f844eed25f4f120a51de6099768fe6749
fortyseven <fortyseven@gmail.com>
2015-05-23 05:56:20 -0400

    Upgraded some variables to constants; made NSNOW more descriptive

--- js/drifter.js
+++ js/drifter.js
@@ -1,14 +1,16 @@
+const NUM_FLAKES = 100;
+const FRAME_DELAY = 75;
+
 var xpos = 50;
 var fg = null, fg2 = null;
-var NSNOW = 100;
 
-var snow_x = new Array( NSNOW );
-var snow_xvel = new Array( NSNOW );
-var snow_y = new Array( NSNOW );
-var snow_yvel = new Array( NSNOW );
-var snow_div = new Array( NSNOW );
-var snow_size = new Array( NSNOW );
-var snow_twist = new Array( NSNOW );
+var snow_x = new Array( NUM_FLAKES );
+var snow_xvel = new Array( NUM_FLAKES );
+var snow_y = new Array( NUM_FLAKES );
+var snow_yvel = new Array( NUM_FLAKES );
+var snow_div = new Array( NUM_FLAKES );
+var snow_size = new Array( NUM_FLAKES );
+var snow_twist = new Array( NUM_FLAKES );
 var c = 0;
 var width = 0, height = 0;
 
@@ -17,7 +19,7 @@ $( document ).ready( function ()
     width = $( window ).width();
     height = $( window ).height();
 
-    for ( var i = 0; i < NSNOW; i++ ) {
+    for ( var i = 0; i < NUM_FLAKES; i++ ) {
         snow_x[ i ] = Math.round( Math.random() * width );
         snow_y[ i ] = Math.round( Math.random() * height );
         snow_xvel[ i ] = (5 + (Math.random() * 20));
@@ -39,14 +41,14 @@ $( document ).ready( function ()
         snow_div[ i ].css( "opacity", Math.random() * 1 );
     }
 
-    setInterval( "updateMove()", 50 );
+    setInterval( updateMove, FRAME_DELAY );
 } );
 
 function updateMove()
 {
     var ox, oy, wo;
 
-    for ( var i = 0; i < NSNOW; i++ ) {
+    for ( var i = 0; i < NUM_FLAKES; i++ ) {
         wo = Math.sin( c * 0.1 ) * (Math.random() * 5);
 
         snow_x[ i ] += (snow_xvel[ i ] + wo);
